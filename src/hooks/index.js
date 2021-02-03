ebad8754f268c37064cfd58353c39e4696c61450
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-02-03 21:48:12 +0900

    Create useProjects hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -46,3 +46,29 @@ export function useTasks(projectId) {
 
   return { tasks, archivedTasks };
 }
+
+export function useProjects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('projects')
+      .where('userId', '==', 'testUser1')
+      .orderBy('projectId')
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
+          ...project.data(),
+          docId: project.id,
+        }));
+
+        // Only set projects when project has been changed to avoid recusion error
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
+
+  return { projects, setProjects };
+}
