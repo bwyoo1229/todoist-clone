9c6500511691fdcc6f47451e679a4e27ca5d74c3
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-02-04 17:09:38 +0900

    Fix SelectedProjectsContext

--- src/contexts/selected-projects-context.js
+++ src/contexts/selected-projects-context.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from 'react';
 export const SelectedProjectsContext = createContext();
 
 export function SelectedProjectsProvider({ children }) {
-  const { selectedProjects, setSelectedProjects } = useState('INBOX');
+  const [selectedProjects, setSelectedProjects] = useState('INBOX');
 
   return (
     <SelectedProjectsContext.Provider
