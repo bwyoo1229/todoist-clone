9b898a24d27970e65d19967e88f4d6f9369a0bf9
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-02-05 11:08:55 +0900

    Create tasks component

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Checkbox } from '.';
 import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { SelectedProjectsContext, ProjectsContext } from '../contexts';
+import { getCollatedTasks, getTitle, getCollatedTitle } from '../helpers';
 
 export function Tasks() {
-  const { tasks } = useTasks('1');
-  console.log(tasks);
+  const { selectedProjects } = useContext(SelectedProjectsContext);
+  const { projects } = useContext(ProjectsContext);
+  const { tasks } = useTasks(selectedProjects);
 
   let projectName = '';
+
+  if (projects && selectedProjects && !getCollatedTasks(selectedProjects)) {
+    projectName = getTitle(projects, selectedProjects).name;
+  }
+
+  if (getCollatedTasks(selectedProjects) && selectedProjects) {
+    projectName = getCollatedTitle(collatedTasks, selectedProjects).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todosit`;
+  });
 
   return (
     <div className="tasks">
